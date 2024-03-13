a795107ea3b9b9fa217ffbee7ebbfd209148aeb4
Ialghi <algi070605@gmail.com>
2024-03-13 20:57:18 +0700

    Praktikum-pekan-4

---
+++ Praktikum/praktikum-pekan-1/array_asosiatif.php
@@ -0,0 +1,12 @@
+<?php
+// arary asosiatif didefinisikan dengan "=>" setelah (key) dan diakhiri dengan "," setelah (nilai)
+$mhs_1 = [
+    'nama' => 'Ihsan Ahmad Al-ghifari', 
+    'nim' => 0110123251, 
+    'jurusan' => 'Sisteminformasi',
+];
+
+//memanggil array asosiatif tidak mengunakan index tapi menggunakan key
+echo 'Nama: ' . $mhs_1['nama'];
+
+?>
