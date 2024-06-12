4470b85fa2f878719609bcdb98eb6b5319550e47
Ialghi <algi070605@gmail.com>
2024-06-12 13:10:17 +0700

    praktikum pekan 13

--- routes/web copy.php
+++ routes/web copy.php
@@ -1,13 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\CourseConteroller;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StudentController;
 use App\Models\Students;
-
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,20 +15,8 @@ Route::get('/', function () {
 
 // routing profil
 
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified']);
-
-Route::middleware('auth')->group(function () {
-
-    //======dashboard route======
-    Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+//======dashboard route======
+Route::get('admin/dashboard', [DashboardController::class, 'index']);
 
 
 
@@ -71,10 +56,3 @@ Route::put('admin/course/update/{id}', [CourseConteroller::class, 'update']);
 
 // course delete
 Route::delete('admin/course/delete/{id}', [CourseConteroller::class, 'destroy']);
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
